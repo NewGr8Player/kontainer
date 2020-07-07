13366268125dddb98a812324125ee27e122610a8
NewGr8Player <273221594@qq.com>
2020-07-07 11:31:01 +0800

    Initial commit

---
+++ src/main/java/com/xavier/kontainer/KontainerApplication.java
@@ -0,0 +1,13 @@
+package com.xavier.kontainer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KontainerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KontainerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/xavier/kontainer/KontainerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xavier.kontainer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KontainerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
